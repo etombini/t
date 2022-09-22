97bc9b9ad0d47ba3e555e6f3375f2db9feaca5ea
Elvis <elvis@mapom.me>
2022-09-22 17:02:31 +0200

    print shell aliases

--- main.go
+++ main.go
@@ -96,52 +96,59 @@ func main() {
 		description: "Print current version.",
 	})
 
+	alias := make(map[string]bool)
+	alias["alias"] = true
+	helps = append(helps, help{
+		verb:        alias,
+		description: "Print alias commands for your shell",
+	})
+
 	switch {
 	case list[os.Args[1]]:
 		err := tl.List(os.Args[2:])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list todo: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list todo: %s\n", err)
 			os.Exit(1)
 		}
 		return
 	case listAt[os.Args[1]]:
 		err := tl.ListAt(os.Args[2:])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list @words: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list @words: %s\n", err)
 			os.Exit(1)
 		}
 		return
 	case listHash[os.Args[1]]:
 		err := tl.ListHash(os.Args[2:])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list #words: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list #words: %s\n", err)
 			os.Exit(1)
 		}
 		return
 	case listPlus[os.Args[1]]:
 		err := tl.ListPlus(os.Args[2:])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list #words: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list #words: %s\n", err)
 			os.Exit(1)
 		}
 		return
 	case delete_[os.Args[1]]:
 		if err := tl.Delete(os.Args[2:]); err != nil {
-			fmt.Fprintf(os.Stderr, "Can not delete todo item: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not delete todo item: %s\n", err)
 			os.Exit(1)
 		}
 		if err := tl.List([]string{}); err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list todo: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list todo: %s\n", err)
 			os.Exit(1)
 		}
 		return
 	case edit[os.Args[1]]:
 		if err := tl.Edit(os.Args[2:]); err != nil {
-			fmt.Fprintf(os.Stderr, "Can not edit todo item: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not edit todo item: %s\n", err)
 			os.Exit(1)
 		}
 		if err := tl.List([]string{}); err != nil {
-			fmt.Fprintf(os.Stderr, "Can not list todo: %s", err)
+			fmt.Fprintf(os.Stderr, "Can not list todo: %s\n", err)
 			os.Exit(1)
 		}
 		return
@@ -152,6 +159,11 @@ func main() {
 	case version[os.Args[1]]:
 		printVersion()
 		return
+	case alias[os.Args[1]]:
+		if err := printAlias(os.Args[2:]); err != nil {
+			fmt.Fprintf(os.Stderr, "Can not print aliases: %s\n", err)
+			os.Exit(1)
+		}
 	default:
 		fmt.Fprintf(os.Stderr, "Unknonw command\n")
 		helps.printHelp()
@@ -160,4 +172,24 @@ func main() {
 
 func printVersion() {
 	fmt.Printf("%s\n", Version)
+}
+
+func printAlias(args []string) error {
+	shell := "bash"
+	if len(args) >= 1 {
+		shell = args[0]
+	}
+
+	switch shell {
+	case "bash":
+		fmt.Println("alias tl=\"t l\"")
+		fmt.Println("alias tl@=\"t l@\"")
+		fmt.Println("alias tl#=\"t l#\"")
+		fmt.Println("alias tl+=\"t l+\"")
+		fmt.Println("alias te=\"t e\"")
+		fmt.Println("alias td=\"t d\"")
+	default:
+		return fmt.Errorf("unsupported shell: %s", shell)
+	}
+	return nil
 }
